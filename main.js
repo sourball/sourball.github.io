e2f1fea3534b42e80176d1436b88474cfc071e2f
sourball <47298079+sourball@users.noreply.github.com>
2019-02-22 17:35:39 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,88 @@
+var compTime = 10;
+var composing = false;
+var d = new Date();
+var seconds = d.getSeconds();
+var compositions = 0;
+var experience = 0;
+var expBrackets = [100, 150, 225, 450, 600];
+var currentLvl = 0;
+var albumsList = [];
+var datesList = [];
+var tblBool = false;
 
+//declare albums table
+var tbl = document.createElement("TABLE");
+tbl.setAttribute("id", "albums");
+tbl.setAttribute("border", "1")
+var tblCaption = tbl.createCaption();
+tblCaption.innerHTML = "Albums: ";
+tblCaption.setAttribute("class", "leftalign");
+var tblRow = tbl.insertRow();
+var cell1 = tblRow.insertCell(0);
+cell1.innerHTML = "<b>Title</b>";
+cell1.setAttribute("class", "initrows");
+var cell2 = tblRow.insertCell(1);
+cell2.innerHTML = "<b>Release date</b>";
+cell2.setAttribute("class", "initrows");
+tblRow.setAttribute("class", "init");
+
+function element(id, action, data, subdata) {
+	switch(action){
+		case "innerHTML":
+			document.getElementById(id).innerHTML = data;
+			break;
+		
+		case "set_a":
+			document.getElementById(id).setAttribute(data, subdata);
+			break;
+		
+		case "rm_a":
+			document.getElementById(id).removeAttribute(data);
+			break;
+	}
+}
+
+function poseFunction() {
+	element("buttonson", "set_a", "disabled", "");
+	var cDown = compTime;
+	element("buttonson", "innerHTML", cDown);
+	var id = setInterval(function(){countTry();}, 1000);
+	function countTry() {					
+		if (cDown == 0){
+			element("buttonson", "innerHTML", "Click me");
+			element("buttonson", "rm_a", "disabled");
+			compositions += 1;
+			experience += (9 + ((Math.floor(Math.random() * Math.floor(50))-25)/10));
+			
+			if (experience > expBrackets[currentLvl]) {experience -= expBrackets[currentLvl]; currentLvl++;}
+			
+			element("counter", "innerHTML", "Songs composed: " + compositions.toString());
+			element("exp", "innerHTML", "Composing level: " + currentLvl.toString() + " (" + experience.toFixed(1) + "/" + expBrackets[currentLvl].toString() + " exp to next level)");
+			clearInterval(id);
+			
+			if (compositions >= 10) { element("create","rm_a","disabled"); }
+		} 
+		else{
+			cDown += -1;
+			document.getElementById("buttonson").innerHTML = cDown; 
+		}
+	}
+	
+}
+function createAlbum() {
+	albumName = prompt("Album name: ");
+	albumsList.push(albumName);
+	datesList.push("not yet released");
+	
+	//if albums table doesn't show, display it
+	if(!tblBool){
+		document.body.appendChild(tbl);
+		
+		tblBool = true;
+	}
+	var albumRow = tbl.insertRow();
+	var titleCell = albumRow.insertCell(0);
+	var dateCell = albumRow.insertCell(1);
+	titleCell.innerHTML = albumName[0];
+	dateCell.innerHTML = datesList[0];
+}
